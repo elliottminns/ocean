8c61041b6255cd304d8589cdb4a640ac59edf965
Elliott Minns <elliott.minns@me.com>
2016-02-26 12:35:55 +0000

    Added in public

--- Sources/Data.swift
+++ Sources/Data.swift
@@ -4,11 +4,13 @@
     import Darwin.C
 #endif
 
-class Data {
-    let bytes: UnsafeMutablePointer<Int8>
-    let capacity: Int
+public class Data {
     
-    init(capacity: Int) {
+    public let bytes: UnsafeMutablePointer<Int8>
+
+    public let capacity: Int
+    
+    public init(capacity: Int) {
         bytes = UnsafeMutablePointer<Int8>(malloc(capacity + 1))
         self.capacity = capacity
     }
@@ -17,13 +19,13 @@ class Data {
         free(bytes)
     }
     
-    var characters: [CChar] {
+    public var characters: [CChar] {
         var data = [CChar](count: capacity, repeatedValue: 0)
         memcpy(&data, bytes, data.count)
         return data
     }
     
-    var string: String? {
+    public var string: String? {
         return String.fromCString(bytes)
     }
 }
